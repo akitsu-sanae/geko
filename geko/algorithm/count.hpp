5ab536c681626cdafc8eb59c9b6a0e0a4ab43cd2
akitsu-sanae <akitsu.sanae@gmail.com>
2016-04-29 16:50:56 +0900

    fix algorithm/count.hpp (not use std::size_t)

--- geko/algorithm/count.hpp
+++ geko/algorithm/count.hpp
@@ -9,18 +9,19 @@
 #ifndef GEKO_ALGORITHM_COUNT_HPP
 #define GEKO_ALGORITHM_COUNT_HPP
 
-#include<cstddef> // std::size_t
+#include <iterator>
 
 namespace geko{
 namespace algorithm{
 
-// TODO: use iterator_traits<InoutIt>::difference_type, not std::size_t
-template<class InputIt,class T>
-inline constexpr std::size_t
-count(InputIt first,InputIt last,const T& value){
-    std::size_t result = 0;
-    while(first!=last){
-        if(*first == value)result++;
+template<typename InputIt, typename T>
+inline constexpr typename std::iterator_traits<InputIt>::difference_type
+count(InputIt first, InputIt last, const T& value){
+    typename std::iterator_traits<InputIt>::difference_type result = 0;
+
+    while (first != last) {
+        if (*first == value)
+            result++;
         ++first;
     }
     return result;
