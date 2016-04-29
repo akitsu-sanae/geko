9f4165b96b25abf42c1d696c2ecae0e3003f2796
akitsu-sanae <akitsu.sanae@gmail.com>
2016-04-29 16:42:31 +0900

    change LICENSE (MIT to Boost) and fix string

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 C++14に対応しているコンパイラじゃないとコンパイルエラー吐きます.  
 現段階で使っているC++14的要素  
 * constexpr関数の制限緩和  
+
+# Copyrights
+Copyright (C) 2015-2016 akitsu sanae.  
+Distributed under the Boost Software License, Version 1.0. 
+(See accompanying file LICENSE or copy at http://www.boost/org/LICENSE_1_0.txt)  
+
--- geko/string.hpp
+++ geko/string.hpp
@@ -1,7 +1,35 @@
+/*============================================================================
+  Copyright (C) 2015-2016 akitsu sanae
+  https://github.com/akitsu-sanae/geko
+  Distributed under the Boost Software License, Version 1.0. (See accompanying
+  file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+============================================================================*/
+
 #ifndef GEKO_STRING_HPP
 #define GEKO_STRING_HPP
 
-#include "string/string.hpp"
+#include <utility>
+
+namespace geko {
+namespace string {
+
+template<std::size_t N>
+struct string {
+    static constexpr std::size_t size = N;
+
+    template<std::size_t ... I>
+    constexpr string(const char* str, std::index_sequence<I ...>) :
+        elems({str[I] ...})
+    {}
+
+    constexpr string(const char* str) :
+        string(str, std::make_index_sequence<N>())
+    {}
+    char elems[N+1];
+};
+
+}
+}
 
 #endif
 
