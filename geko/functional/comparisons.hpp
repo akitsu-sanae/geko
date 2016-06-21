23c51b0d98860de2fbaa2a240790ebdbd00ffdb9
akitsu-sanae <akitsu.sanae@gmail.com>
2016-06-22 08:22:44 +0900

    add functional comparisons

---
+++ geko/functional/comparisons.hpp
@@ -0,0 +1,19 @@
+/*============================================================================
+  Copyright (C) 2015-2016 akitsu sanae
+  https://github.com/akitsu-sanae/geko
+  Distributed under the Boost Software License, Version 1.0. (See accompanying
+  file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+============================================================================*/
+
+#ifndef GEKO_FUNCTIONAL_COMPARISON_HPP
+#define GEKO_FUNCTIONAL_COMPARISON_HPP
+
+#include <geko/functional/equal_to.hpp>
+#include <geko/functional/not_equal_to.hpp>
+#include <geko/functional/less.hpp>
+#include <geko/functional/less_equal.hpp>
+#include <geko/functional/greater.hpp>
+#include <geko/functional/greater_equal.hpp>
+
+#endif
+
---
+++ geko/functional/equal_to.hpp
@@ -0,0 +1,37 @@
+/*============================================================================
+  Copyright (C) 2015-2016 akitsu sanae
+  https://github.com/akitsu-sanae/geko
+  Distributed under the Boost Software License, Version 1.0. (See accompanying
+  file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+============================================================================*/
+
+#ifndef GEKO_FUNCTIONAL_EQUAL_TO_HPP
+#define GEKO_FUNCTIONAL_EQUAL_TO_HPP
+
+namespace geko {
+namespace functional {
+
+template<typename T = void>
+struct equal_to {
+    using first_arument_type = T;
+    using second_arugment_type = T;
+    using result_type = bool;
+    constexpr result_type operato()(T const& lhs, T const& rhs) const {
+        return lhs == rhs;
+    }
+};
+
+template<>
+struct equal_to<void> {
+    using is_transparent = void;
+    template<typename T, typename U>
+    auto operato()(T&& lhs, U&& rhs) const {
+        return lhs == rhs;
+    }
+};
+
+}
+}
+
+#endif
+
