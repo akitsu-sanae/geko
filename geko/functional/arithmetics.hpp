23e0196600ccb246a578dcb3a4752e9c7b3096a3
akitsu-sanae <akitsu.sanae@gmail.com>
2016-06-22 08:05:57 +0900

    add functional arithmetics

---
+++ geko/functional/arithmetics.hpp
@@ -0,0 +1,19 @@
+/*============================================================================
+  Copyright (C) 2015-2016 akitsu sanae
+  https://github.com/akitsu-sanae/geko
+  Distributed under the Boost Software License, Version 1.0. (See accompanying
+  file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+============================================================================*/
+
+#ifndef GEKO_FUNCTIONAL_ARITHMETICS_HPP
+#define GEKO_FUNCTIONAL_ARITHMETICS_HPP
+
+#include <geko/functional/plus.hpp>
+#include <geko/functional/minus.hpp>
+#include <geko/functional/multiplies.hpp>
+#include <geko/functional/divides.hpp>
+#include <geko/functional/modulus.hpp>
+#include <geko/functional/negate.hpp>
+
+#endif
+
