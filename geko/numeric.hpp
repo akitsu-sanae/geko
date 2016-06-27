c5ddaef73ff3ce3eed74ab026946f113708f9612
akitsu-sanae <akitsu.sanae@gmail.com>
2016-06-27 17:40:42 +0900

    add numeric

---
+++ geko/numeric.hpp
@@ -0,0 +1,15 @@
+/*============================================================================
+  Copyright (C) 2015-2016 akitsu sanae
+  https://github.com/akitsu-sanae/geko
+  Distributed under the Boost Software License, Version 1.0. (See accompanying
+  file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+============================================================================*/
+
+#ifndef GEKO_NUMERIC_HPP
+#define GEKO_NUMERIC_HPP
+
+#include <geko/numeric/accumulate.hpp>
+#include <geko/numeric/inner_producct.hpp>
+
+#endif
+
