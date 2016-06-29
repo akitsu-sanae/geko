308cb7560c855af021c9ac58f5157ac3dea3d488
akitsu-sanae <akitsu.sanae@gmail.com>
2016-06-30 00:30:59 +0900

    add iterator tags

---
+++ geko/iterator.hpp
@@ -0,0 +1,13 @@
+/*============================================================================
+  Copyright (C) 2015-2016 akitsu sanae
+  https://github.com/akitsu-sanae/geko
+  Distributed under the Boost Software License, Version 1.0. (See accompanying
+  file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+============================================================================*/
+
+#ifndef GEKO_ITERATOR_HPP
+#define GEKO_ITERATOR_HPP
+
+#include <geko/iterator/tags.hpp>
+
+#endif
---
+++ geko/iterator/tags.hpp
@@ -0,0 +1,26 @@
+/*============================================================================
+  Copyright (C) 2015-2016 akitsu sanae
+  https://github.com/akitsu-sanae/geko
+  Distributed under the Boost Software License, Version 1.0. (See accompanying
+  file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+============================================================================*/
+
+
+#ifndef GEKO_ITERATOR_TAGS_HPP
+#define GEKO_ITERATOR_TAGS_HPP
+
+namespace geko {
+namespace iterator {
+
+struct input_iterator_tag {};
+struct output_iterator_tag {};
+struct forward_iterator_tag : public input_iterator_tag {};
+struct bidirectional_iterator_tag : public forward_iterator_tag {};
+struct random_access_iterator_tag : public bidirectional_iterator_tag {};
+
+
+}
+}
+
+#endif
+
