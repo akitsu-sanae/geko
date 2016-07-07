ac3d4152d638acb1f1e758c038597d174aa7ff3f
akitsu-sanae <akitsu.sanae@gmail.com>
2016-07-07 11:15:58 +0900

    add move

---
+++ geko/utility/move.hpp
@@ -0,0 +1,26 @@
+/*============================================================================
+  Copyright (C) 2015-2016 akitsu sanae
+  https://github.com/akitsu-sanae/geko
+  Distributed under the Boost Software License, Version 1.0. (See accompanying
+  file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+============================================================================*/
+
+
+#ifndef GEKO_UTILITY_HPP
+#define GEKO_UTILITY_HPP
+
+#include <type_traits>
+
+namespace geko {
+namespace utility {
+
+template<typename T>
+inline constexpr typename std::remove_reference<T>::type&&
+move(T&& t) {
+    return static_cast<typename std::remove_reference<T>::type&&>(t);
+}
+
+}
+}
+
+#endif
