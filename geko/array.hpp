3925cfeed8ddc0c1255d39843c166fce94cf0434
akitsu-sanae <akitsu.sanae@gmail.com>
2016-07-10 14:31:04 +0900

    array

--- geko/array.hpp
+++ geko/array.hpp
@@ -25,6 +25,47 @@ struct array {
     using pointer = T*;
     using const_pointer = T const*;
 
+
+    struct out_of_range {};
+
+    reference at(size_type pos) {
+        if (pos < 0 || pos > static_size)
+            throw out_of_range{};
+        return elems[pos];
+    }
+    const_reference at(size_type pos) const {
+        if (pos < 0 || pos > static_size)
+            throw out_of_range{};
+        return elems[pos];
+    }
+    reference operator[](size_type pos) {
+        return at(pos);
+    }
+    const_reference operator[](size_type pos) const {
+        return at(pos);
+    }
+
+    reference front() {
+        return at(static_cast<size_type>(0));
+    }
+    const_reference front() const {
+        return at(static_cast<size_type>(0));
+    }
+
+    reference back() {
+        return at(static_cast<size_type>(static_size - 1));
+    }
+    const_reference back() const {
+        return at(static_cast<size_type>(static_size -1));
+    }
+
+    value_type* data() {
+        return elems;
+    }
+    const value_type* data() const {
+        return elems;
+    }
+
     constexpr size_type size() const noexcept {
         return static_size;
     }
@@ -35,11 +76,33 @@ struct array {
     constexpr const_iterator begin() const noexcept {
         return iterator(elems);
     }
+    constexpr const_iterator cbegin() const noexcept {
+        return iterator(elems);
+    }
     constexpr iterator end() noexcept {
         return iterator(elems) + size();
     }
     constexpr const_iterator end() const noexcept {
         return iterator(elems) + size();
+    }
+    constexpr const_iterator cend() const noexcept {
+        return iterator(elems) + size();
+    }
+
+    constexpr bool empty() const noexcept {
+        return static_size == 0;
+    }
+
+    void fill(T const& value) {
+        for (auto&& e : elems)
+            e = value;
+    }
+    void swap(array& other) noexcept {
+        for (size_type i=0; i<static_size; i++) {
+            value_type tmp = elems[i];
+            elems[i] = other.at(i);
+            other.at(i) = std::move(tmp);
+        }
     }
 
     static constexpr std::size_t static_size = N;
